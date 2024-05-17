34866bc32d297771277cbc2a91dfe05a4378b5ef
Arun Rathore <arathore@nowoptics.com>
2024-05-17 11:39:08 +0530

    Initial Commit

---
+++ CounterUsingTCA/CounterUsingTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  CounterUsingTCAApp.swift
+//  CounterUsingTCA
+//
+//  Created by Arun Rathore on 17/05/24.
+//
+
+import SwiftUI
+
+@main
+struct CounterUsingTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
